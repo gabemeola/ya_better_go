a8ae19e3503225b4bf42a060aa133bdfe0dbc03c
Gabe M <gabemeola@gmail.com>
2018-02-18 20:50:55 -0700

    added Map and List

---
+++ templates/map/map.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.html"))
+}
+
+func main() {
+	sages := map[string]string{
+		"India":    "Gandhi",
+		"America":  "MLK",
+		"Meditate": "Buddha",
+		"Love":     "Jesus",
+		"Prophet":  "Muhammad",
+	}
+
+	err := tpl.Execute(os.Stdout, sages)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
